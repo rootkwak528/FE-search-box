ded4e7c2559025543ed5ac94e1013c83e0016218
Root_Kwak <rootkwak528@gmail.com>
2021-05-12 21:51:27 +0900

    feat: TMDB API 검색 - 검색 기능 API 연결

--- src/views/Tmdb.vue
+++ src/views/Tmdb.vue
@@ -7,7 +7,12 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 import TmdbSearchBox from '@/components/TmdbSearchBox'
+
+const API_URL = 'https://api.themoviedb.org/3'
+const API_KEY = process.env.VUE_APP_TMDB_API_KEY
 
 export default {
   name: 'Tmdb',
@@ -20,9 +25,33 @@ export default {
     TmdbSearchBox
   },
   methods: {
-    onTmdbTextInput (textInput) {
-      console.log(textInput)
+    getUrl (category='movie', feature='', kwargs) {
+      let url = `${API_URL}/${category}/${feature}?api_key=${API_KEY}`
+      for (const key in kwargs) {
+        url += `&${key}=${kwargs[key]}`
+      }
+
+      return url
     },
+    onTmdbTextInput (textInput) {
+      this.searchTmdb(textInput)
+    },
+    searchTmdb (searchKeyword) {
+      const params = {
+        region: 'KR',
+        language: 'ko',
+        query: searchKeyword
+      }
+      const url = this.getUrl('search', 'movie', params)
+
+      axios({
+        method: 'get',
+        url
+      })
+      .then((res) => {
+        console.log(res)
+      })
+    }
   }
 }
 </script>
