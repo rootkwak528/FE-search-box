5836d08876f3bf3f13e8b63a84931e01b5dcec79
Root_Kwak <rootkwak528@gmail.com>
2021-05-12 22:04:48 +0900

    feat: TMDB 검색 결과 출력 - 리스트 컴포넌트에 pass props

---
+++ src/components/TmdbSearchList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TmdbSearchList',
+  props: {
+    movieList: [String, Array]
+  },
+}
+</script>
+
+<style>
+
+</style>
